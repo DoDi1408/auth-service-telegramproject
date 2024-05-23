5283b41ece97a9ec9d49c764d119da4612bed144
DoDi1408 <ro.chavez.5454@gmail.com>
2024-05-22 18:03:03 -0600

    added k8s secret to have a unique and constant private key

--- src/main/java/com/authservice/service/JWTService.java
+++ src/main/java/com/authservice/service/JWTService.java
@@ -4,7 +4,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.authservice.models.Employee;
@@ -15,11 +19,20 @@ import io.jsonwebtoken.Claims;
 
 @Component
 public class JWTService {
+    private final static Logger loggerJWT = LoggerFactory.getLogger(JWTService.class);
+    private SecretKey pvKey;
+    //private final SecretKey pvKey2 = Jwts.SIG.HS512.key().build();
 
-    private final SecretKey pvKey = Jwts.SIG.HS512.key().build();
-
+    public JWTService(){
+        try {
+            pvKey = SecretKeyFactory.getInstance("HmacSHA512").generateSecret(new SecretKeySpec(System.getenv("SIGNING-KEY").getBytes(), "HmacSHA512"));
+        }
+        catch(Exception e){
+            pvKey = Jwts.SIG.HS512.key().build();
+        }
+    }
     public String createJWT(Employee emp){
-
+        loggerJWT.info("Creating JWT");
         Date currentTime = new Date();
         Date expirationTime = new Date(currentTime.getTime() + TimeUnit.HOURS.toMillis(3600000));
 
@@ -41,6 +54,7 @@ public class JWTService {
     }
 
     public Employee parseJwtToEmployee(String jwt){
+        loggerJWT.info("parsing jwt");
         Jws<Claims> parsedJwt = Jwts.parser().verifyWith(pvKey).build().parseSignedClaims(jwt);
         Claims claims = parsedJwt.getPayload();
         Employee returnEmployee = new Employee(claims.get("firstName", String.class),claims.get("lastName", String.class),claims.get("telegramId", Integer.class));
